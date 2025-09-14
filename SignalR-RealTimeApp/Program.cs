a674330c2b37fca88e0304212130d0ab7dbecdd4
ahmed elkomy <ahmedelkomy.2889@gmail.com>
2025-09-14 21:48:50 +0300

    Add Cors

--- SignalR-RealTimeApp/Program.cs
+++ SignalR-RealTimeApp/Program.cs
@@ -1,4 +1,4 @@
-using SignalR_RealTimeApp.Hubs;
+﻿using SignalR_RealTimeApp.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,8 +8,19 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddSignalR();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp",
+        builder => builder
+            .WithOrigins("http://localhost:3000") // رابط React
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials());
+});
 
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -20,7 +31,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
+app.UseCors("AllowReactApp");
 app.MapHub<ChatHub>("/chathub");
 
 app.MapControllers();
